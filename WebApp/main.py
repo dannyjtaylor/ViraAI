e28033bdc64e3bb96e81da7e6256c8d8af44be43
Daniel Taylor <146564443+dannyjtaylor@users.noreply.github.com>
2025-06-20 09:51:50 -0400

    trying to fix routing traffic error

--- WebApp/main.py
+++ WebApp/main.py
@@ -196,3 +196,9 @@ async def ask(request: Request):
     conn.commit()
 
     return {"response": content, "time": timestamp}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
